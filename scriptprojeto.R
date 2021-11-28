4efaad0c60a2bd7d5281b0886822f7be7bd97481
unknown <helcio_sasaki@yahoo.com>
2021-11-28 12:10:10 -0300

    segunda nvas versao commit

--- scriptprojeto.R
+++ scriptprojeto.R
@@ -1,5 +1,10 @@
 # Version 1 
 
 library("tidyverse")
-library
+
+library(readxl)
+dataset_inicial <- read_excel("C:/Users/Helcio/Desktop/MBA Esalq/2021.11.09 - Data Wrangling/(1.2) Dataset Aula Data Wrangling.xls")
+
+
+
 
