cf4d74edba3ef36715ae75998693febfa169d017
hsasaki10 <50645769+hsasaki10@users.noreply.github.com>
2021-11-28 12:12:58 -0300

    Update scriptprojeto.R

--- scriptprojeto.R
+++ scriptprojeto.R
@@ -5,7 +5,8 @@ library("tidyverse")
 library(readxl)
 dataset_inicial <- read_excel("C:/Users/Helcio/Desktop/MBA Esalq/2021.11.09 - Data Wrangling/(1.2) Dataset Aula Data Wrangling.xls")
 
-renome()
-
+rename()
+recode()
+mutate()
 
 
